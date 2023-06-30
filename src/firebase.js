0a3646ffb9cd198bef7a832eab26d08e5c7ccf5a
Arunprasath2003 <arunprasath.b2003@gmail.com>
2023-06-30 21:21:27 +0530

    frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDML4PME3jWuk8ADblIM3-X5A_i8r8Aui4",
+    authDomain: "whatsapp-clone-bd4ef.firebaseapp.com",
+    projectId: "whatsapp-clone-bd4ef",
+    storageBucket: "whatsapp-clone-bd4ef.appspot.com",
+    messagingSenderId: "476973940942",
+    appId: "1:476973940942:web:b10f197fddddad863a109f"
+  };
